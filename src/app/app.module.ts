e6834619edc4817b9573ca40cce68067130d6f15
kocend <48856972+kocend@users.noreply.github.com>
2020-04-06 15:04:28 +0200

    Add Reactive Form for register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -50,6 +50,7 @@ import { MyTipsComponent } from './components/my-tips/my-tips.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     TabMenuModule,
     DropdownModule,
     DialogModule,
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,104 +3,101 @@ import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { RegisterRequest } from 'src/app/models/register-request';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors, ValidatorFn, FormControl, AsyncValidatorFn } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
-  providers: [MessageService]
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
+    providers: [MessageService]
 })
 export class RegisterComponent implements OnInit {
 
+    public formGroup: FormGroup;
+
     public displayRegisterDialog: boolean = true;
-
-    public passwordsEqual: boolean = true;
-    public usernameFree: boolean = true;
-    public passwordLongEnough: boolean = true;
-    public allInputsFilled: boolean = true;
-
-    public username: string;
-    public password1: string;
-    public password2: string;
 
     constructor(private authService: AuthService,
                 private router: Router,
-                private messageService: MessageService) { }
+                private messageService: MessageService,
+                private formBuilder: FormBuilder) { }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.formGroup = new FormGroup({
+            username: new FormControl('', Validators.required, this.validUsernameFreedoom(this.authService)),
+            password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+            passwordConfirmation: new FormControl('', [Validators.required, Validators.minLength(4)])
+        }, [ this.validPasswordsEquality('password', 'passwordConfirmation')]);
+    }
 
     public signUp(): void {
 
-        if(this.areAllInputsFilled())
-        if(this.arePasswordsEqual())
-        if(this.isPasswordLongEnough())
-            this.authService.isUsernameFree(this.username)
-                .subscribe(result => {
-                    if(this.usernameFree = result.isFree){
-                        console.log("username is free");
-                        let userdata = new RegisterRequest;
-                        userdata.username = this.username;
-                        userdata.password = this.password1
-                        this.authService.register(userdata)
-                            .subscribe(result => {
-                                    this.displayRegisterDialog = false;
-                                    this.router.navigate(['/login']);
-                                    this.messageService.add({ severity: 'success', summary: 'Pomyślna Rejestracja', detail: ""});
-                            })
-                    }
+        let userdata = new RegisterRequest;
+        userdata.username = this.usernameFormControl.value;
+        userdata.password = this.passwordFormControl.value;
+        this.authService.register(userdata)
+            .subscribe(result => {
+                this.displayRegisterDialog = false;
+                this.router.navigate(['/login']);
+                this.messageService.add({ severity: 'success', summary: 'Pomyślna Rejestracja', detail: "" });
+            })
+    }
+
+    private validUsernameFreedoom(authService: AuthService): AsyncValidatorFn {
+        return async (c: AbstractControl): Promise<ValidationErrors | null> => {
+            const usernameValue = c.value;
+
+            if(usernameValue?.trim() == 0)
+                return;
+
+            let isFree: boolean = false;
+
+            await authService.isUsernameFree(usernameValue)
+                .toPromise()
+                .then(result => {
+                    isFree = result.isFree;
+            });
+
+            if (isFree)
+                return null;
+            else
+                return new Promise((resolve) => {
+                    resolve({ 'notFree': true });
                 });
-    }
-
-    public resetValidation(){
-        console.log("resetting validation ...");
-        this.passwordsEqual = true;
-        this.usernameFree = true;
-        this.passwordLongEnough = true;
-        this.allInputsFilled = true;
-    }
-
-    private areAllInputsFilled(): boolean {
-        console.log("areAllInputsFilled()");
-        if(this.username?.trim().length > 0 &&
-            this.password1?.trim().length > 0 &&
-            this.password2?.trim().length > 0){
-                console.log("yes");
-                this.allInputsFilled = true;
-                return true;
-        }
-        else{
-            console.log("no");
-            this.allInputsFilled = false;
-            return false;
         }
     }
 
-    private arePasswordsEqual(): boolean {
-        console.log("arePasswordsEqual()");
-        if(this.password1 === this.password2){
-            console.log("equal");
-            this.passwordsEqual = true;
-            return true;
-        }
-        else{
-            console.log("not equal");
-            this.passwordsEqual = false;
-            return false;
-        }
+    public validPasswordsEquality(password: string, passwordConformation: string): ValidatorFn {
+        return (c: AbstractControl): ValidationErrors | null => {
+            const passwordControl = c.get(password);
+            const passwordConformationControl = c.get(passwordConformation);
+
+            if (passwordControl == null)
+                throw new Error(`Control with name '${password}' not found`);
+
+            if (passwordConformationControl == null)
+                throw new Error(`Control with name '${passwordConformation}' not found`);
+
+            const passwordValue = passwordControl.value;
+            const passwordConformationValue = passwordConformationControl.value;
+
+            if (passwordValue !== passwordConformationValue)
+                return { 'passwordsNotEqual': true };
+            else
+                return null;
+        };
     }
 
-    private isPasswordLongEnough(): boolean {
-        console.log("isPasswordLongEnough()");
-        if(this.password1?.trim().length >= 4){
-            console.log("yes");
-            this.passwordLongEnough = true;
-            return true;
-        }
-        else{
-            console.log("no");
-            this.passwordLongEnough = false;
-            return false;
-        }
+    get usernameFormControl() {
+        return this.formGroup.get('username');
     }
 
+    get passwordFormControl() {
+        return this.formGroup.get('password');
+    }
+
+    get passwordConfirmationFormControl() {
+        return this.formGroup.get('passwordConfirmation');
+    }
 }
